4bc7e4ad901da10d14f7ac34ba2b41422d373d06
Dealga McArdle <zeffii@users.noreply.github.com>
2022-06-13 13:48:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Console Print',
+      version='1.0',
+      description='Print to Blender console',
+      author='Mr Blobby',
+      author_email='blobby@noelshouseparty.bbc',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=[],
+     )
 print("PSYCHE")
